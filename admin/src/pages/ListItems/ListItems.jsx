b6ec112d5b532f8f988246bec19c734d23b9d220
Shehan Lakshitha <shehanlaksh@gmail.com>
2024-07-31 22:48:06 +0530

    Update ListItems component styling and structure

--- admin/src/pages/ListItems/ListItems.jsx
+++ admin/src/pages/ListItems/ListItems.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
+import "./ListItems.css";
+import { assets } from "../../assets/assets.js";
 
 const ListItems = () => {
   return (
-    <div>ListItems</div>
-  )
-}
+    <div className="list_Items">
+      <h3 className="list_title">All List Items</h3>
 
-export default ListItems
+      <table className="list_table">
+        <thead className="table_header">
+          <tr className="header_row">
+            <th scope="col" className="header_name">
+              Image
+            </th>
+            <th scope="col" className="header_name">
+              Name
+            </th>
+            <th scope="col" className="header_name">
+              Category
+            </th>
+            <th scope="col" className="header_name">
+              Description
+            </th>
+            <th scope="col" className="header_name">
+              Price
+            </th>
+            <th scope="col" className="header_name">
+              Delete
+            </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr className="body_row">
+            <td>
+              <img src={assets.parcel_icon} alt="" />
+            </td>
+            <td>
+              Nestamolt
+            </td>
+            <td>
+            Nestamolt
+            </td>
+            <td>
+            Nestamolt
+            </td>
+            <td>
+            Nestamolt
+            </td>
+            <td>
+            Nestamolt
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ListItems;
