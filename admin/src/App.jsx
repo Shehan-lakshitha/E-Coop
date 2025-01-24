e130670fe2cfcbb4a128418779acd0b81d76cf54
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2025-01-24 09:34:50 +0530

    style: improve HTML structure and CSS formatting for consistency

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,25 +1,25 @@
-import React from "react";
-import {Route, Routes} from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-import Sidebar from "./components/Sidebar/Sidebar";
-import AddItems from "./pages/AddItems/AddItems";
-import ListItems from "./pages/ListItems/ListItems";
-import Orders from "./pages/Orders/Orders";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
+import AddItems from './pages/AddItems/AddItems';
+import ListItems from './pages/ListItems/ListItems';
+import Orders from './pages/Orders/Orders';
 
 const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <div className="app-content">
-        <Sidebar />
-        <Routes>
-          <Route path="/addItems" element={<AddItems />} />
-          <Route path="/listItems" element={<ListItems />} />
-          <Route path="/orders" element={<Orders />} />
-        </Routes>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar />
+            <div className="app-content">
+                <Sidebar />
+                <Routes>
+                    <Route path="/addItems" element={<AddItems />} />
+                    <Route path="/listItems" element={<ListItems />} />
+                    <Route path="/orders" element={<Orders />} />
+                </Routes>
+            </div>
+        </div>
+    );
 };
 
 export default App;
--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import './Navbar.css'
-import { assets} from '../../assets/assets.js';
+import React from 'react';
+import './Navbar.css';
+import { assets } from '../../assets/assets.js';
 
 const Navbar = () => {
-  return (
-    <div className="navbar">
-        <img className='logo' src={assets.logo} alt="" />
-        <div className='nav-name'>
-            <h3>Shehan Lakshitha</h3>
-            <img src={assets.profile_image} alt="" className="profile" />
+    return (
+        <div className="navbar">
+            <img className="logo" src={assets.logo} alt="E-COOP" />
+            <div className="nav-name">
+                <h3>Shehan Lakshitha</h3>
+                <img src={assets.profile_image} alt="Profile-Image" className="profile" />
+            </div>
         </div>
-        
-    </div>
-  )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
