a63001b951bc4aa4ea927772c730c0448c2ce2e9
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2025-01-24 10:03:34 +0530

    style: enhance formatting and consistency in AddItems and ListItems components

--- admin/src/pages/ListItems/ListItems.jsx
+++ admin/src/pages/ListItems/ListItems.jsx
@@ -1,61 +1,51 @@
-import React from "react";
-import "./ListItems.css";
-import { assets } from "../../assets/assets.js";
+import React from 'react';
+import './ListItems.css';
+import { assets } from '../../assets/assets.js';
 
 const ListItems = () => {
-  return (
-    <div className="list_Items">
-      <h3 className="list_title">All List Items</h3>
+    return (
+        <div className="list_Items">
+            <h3 className="list_title">All List Items</h3>
 
-      <table className="list_table">
-        <thead className="table_header">
-          <tr className="header_row">
-            <th scope="col" className="header_name">
-              Image
-            </th>
-            <th scope="col" className="header_name">
-              Name
-            </th>
-            <th scope="col" className="header_name">
-              Category
-            </th>
-            <th scope="col" className="header_name">
-              Description
-            </th>
-            <th scope="col" className="header_name">
-              Price
-            </th>
-            <th scope="col" className="header_name">
-              Delete
-            </th>
-          </tr>
-        </thead>
+            <table className="list_table">
+                <thead className="table_header">
+                    <tr className="header_row">
+                        <th scope="col" className="header_name">
+                            Image
+                        </th>
+                        <th scope="col" className="header_name">
+                            Name
+                        </th>
+                        <th scope="col" className="header_name">
+                            Category
+                        </th>
+                        <th scope="col" className="header_name">
+                            Description
+                        </th>
+                        <th scope="col" className="header_name">
+                            Price
+                        </th>
+                        <th scope="col" className="header_name">
+                            Delete
+                        </th>
+                    </tr>
+                </thead>
 
-        <tbody>
-          <tr className="body_row">
-            <td>
-              <img src={assets.parcel_icon} alt="" />
-            </td>
-            <td>
-              Nestamolt
-            </td>
-            <td>
-            Nestamolt
-            </td>
-            <td>
-            Nestamolt
-            </td>
-            <td>
-            Nestamolt
-            </td>
-            <td>
-            Nestamolt
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+                <tbody>
+                    <tr className="body_row">
+                        <td>
+                            <img src={assets.parcel_icon} alt="" />
+                        </td>
+                        <td>Nestamolt</td>
+                        <td>Nestamolt</td>
+                        <td>Nestamolt</td>
+                        <td>Nestamolt</td>
+                        <td>Nestamolt</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
 };
 
 export default ListItems;
