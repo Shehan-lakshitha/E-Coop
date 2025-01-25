92ff791f80987a1a7505b819c0603e863b7c006d
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2025-01-25 12:25:19 +0530

    feat: add styling for login page and enhance button styles in AddItems component

--- admin/src/pages/Login/Login.jsx
+++ admin/src/pages/Login/Login.jsx
@@ -1,37 +1,31 @@
 import React, { useState } from 'react';
 import './Login.css';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
-// import FailedMsg from '../components/failed-msg';
-import { useNavigate } from 'react-router-dom';
-// import LoginBg from '../assets/images/login/login-signup-image.jpg';
 import { IoIosEye } from 'react-icons/io';
 import { IoIosEyeOff } from 'react-icons/io';
+import { toast } from 'react-toastify';
 
 const Login = () => {
-    const navigate = useNavigate();
-    const [formData, setFormData] = useState({
+    const [data, setData] = useState({
         email: '',
         password: '',
     });
-
     const [showPassword, setShowPassword] = useState(false);
     const [errors, setErrors] = useState({});
-    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const handleInputChanges = (e) => {
         const { id, value } = e.target;
-        setFormData((prev) => ({ ...prev, [id]: value }));
+        setData((prev) => ({ ...prev, [id]: value }));
     };
 
-    const validateForm = (formData) => {
+    const validateForm = (data) => {
         const errors = {};
 
-        if (!formData.email.trim()) {
+        if (!data.email) {
             errors.email = 'Email is required';
         }
 
-        if (!formData.password) {
+        if (!data.password) {
             errors.password = 'Password is required';
         }
 
@@ -40,137 +34,101 @@ const Login = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const errors = validateForm(formData);
+        const errors = validateForm(data);
         if (Object.keys(errors).length > 0) {
             return setErrors(errors);
         }
 
+        const url = 'http://localhost:4000';
+
+        const formData = new FormData();
+        formData.append('email', data.email);
+        formData.append('password', data.password);
+
+        for (let pair of formData.entries()) {
+            console.log(`${pair[0]}: ${pair[1]}`);
+        }
+
         try {
             const response = await axios.post(
-                'https://food-delivery-system-for-gather-and-grab-kzp59bwbm.vercel.app/api/auth/login',
-                formData
-            );
-            console.log(response);
-
-            if (response.status === 200) {
-                alert('Login Successful!');
-                localStorage.setItem('authToken', response.data.token);
-                localStorage.setItem('role', response.data.role);
-
-                if (
-                    response.data.role === 'admin' ||
-                    response.data.role === 'super admin'
-                ) {
-                    navigate('/dashboard');
-                } else if (response.data.role === 'user') {
-                    navigate('/');
+                `${url}/api/users/login`,
+                formData,
+                {
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                    },
                 }
+            );
+
+            if (response.data.success) {
+                toast.success(response.data.message);
+                localStorage.setItem('authToken', response.data.token);
+            } else {
+                toast.error(response.data.message);
             }
         } catch (err) {
             console.log(err);
-            setIsModalOpen(true);
         }
     };
 
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
-
     return (
-        <div
-            className="flex items-center min-h-screen justify-center bg-cover bg-center"
-            // style={{ backgroundImage: `url(${LoginBg})` }}
-        >
-            <div className="z-10 items-center justify-center">
-                {/* <FailedMsg
-                    isOpen={isModalOpen}
-                    onClose={closeModal}
-                    title="Invalid Credentials"
-                    message="Try Again!"
-                    link={<Link to={'/login'}>Go to Dashboard</Link>}
-                /> */}
-            </div>
-            <div
-                className={`w-full max-w-4xl p-3   m-2 border-solid rounded-lg shadow-lg sm:pt-14 pb-10 sm:m-6 bg-white/10 backdrop-blur-lg lg:w-1/2 xl:w-1/3 ${
-                    isModalOpen ? 'pointer-events-none blur-sm' : ''
-                }`}
-            >
-                <h1 className="font-bold text-lg sm:text-2xl text-center text-[#fdb560] mb-8 font-poppins">
-                    WELCOME BACK TO YUMZY
-                </h1>
-                <div className="sm:p-8 p-4 pb-10 pt-10 shadow-lg rounded-[50px] bg-white/20">
-                    <h3 className="pb-5 font-semibold text-center text-black sm:text-xl lg:text-lg">
-                        Login!
-                    </h3>
-                    <form
-                        method="POST"
-                        className="space-y-4 "
-                        onSubmit={handleSubmit}
-                        noValidate
-                    >
-                        <div className="form-details">
-                            <label htmlFor="email" className="email">
-                                Email
-                            </label>
-                            <div className="">
-                                <input
-                                    type="text"
-                                    id="email"
-                                    value={formData.email}
-                                    onChange={handleInputChanges}
-                                    className=""
-                                    placeholder="Enter your email"
-                                />
-                                {errors.email && (
-                                    <div className="error-message">
-                                        {errors.email}
-                                    </div>
-                                )}
-                            </div>
-                        </div>
-
+        <div className="login-page">
+            <h1 className="header">WELCOME BACK TO E-COOP ADMIN PANEL</h1>
+            <div className="login-form">
+                <h3>Login!</h3>
+                <form method="POST" onSubmit={handleSubmit}>
+                    <div className="form-details">
+                        <label htmlFor="email">Email</label>
                         <div>
-                            <div className="">
-                                <label htmlFor="password" className="password">
-                                    Password
-                                </label>
-                                <input
-                                    type={showPassword ? 'text' : 'password'}
-                                    id="password"
-                                    value={formData.password}
-                                    onChange={handleInputChanges}
-                                    className=""
-                                    placeholder="Enter your password"
-                                />
-                                <button
-                                    type="button"
-                                    className=""
-                                    onClick={() =>
-                                        setShowPassword(!showPassword)
-                                    }
-                                >
-                                    {showPassword ? (
-                                        <IoIosEye />
-                                    ) : (
-                                        <IoIosEyeOff />
-                                    )}
-                                </button>
-                            </div>
-                            {errors.password && (
+                            <input
+                                type="text"
+                                id="email"
+                                name="email"
+                                value={data.email}
+                                onChange={handleInputChanges}
+                                className={errors.email ? 'error' : ''}
+                                placeholder="Enter your email"
+                            />
+                            {errors.email && (
                                 <div className="error-message">
-                                    {errors.password}
+                                    {errors.email}
                                 </div>
                             )}
                         </div>
+                    </div>
 
-                        <button
-                            type="submit"
-                            className="w-full bg-[#c59d5f] hover:bg-[#fccb45] rounded-lg p-2 text-sm sm:text-lg"
-                        >
-                            Login
-                        </button>
+                    <div className="form-details">
+                        <label htmlFor="password">Password</label>
+                        <div className="password-input">
+                            <input
+                                type={showPassword ? 'text' : 'password'}
+                                name="password"
+                                id="password"
+                                value={data.password}
+                                onChange={handleInputChanges}
+                                className={errors.password ? 'error' : ''}
+                                placeholder="Enter your password"
+                            />
+                            <button
+                                type="button"
+                                className="show-password"
+                                onClick={() => setShowPassword(!showPassword)}
+                            >
+                                {showPassword ? <IoIosEye /> : <IoIosEyeOff />}
+                            </button>
+                        </div>
+                        {errors.password && (
+                            <div className="error-message">
+                                {errors.password}
+                            </div>
+                        )}
+                    </div>
 
-                        <p className="text-sm text-center text-white sm:text-lg">
+                    <button type="submit" className="login-btn">
+                        Login
+                    </button>
+
+                    {/* <p className="text-sm text-center text-white sm:text-lg">
                             Not a member?{' '}
                             <Link
                                 to="/registration"
@@ -178,11 +136,11 @@ const Login = () => {
                             >
                                 Signup
                             </Link>
-                        </p>
-                    </form>
-                </div>
+                        </p> */}
+                </form>
             </div>
         </div>
+        // </div>
     );
 };
 
