fbd17f35346537dc77dc78aa0b22bc488669201b
Shehan Lakshitha <shehanLaksh@gmail.com>
2025-01-27 15:49:30 +0530

    Fixed addproduct errors

--- admin/src/pages/ListItems/ListItems.jsx
+++ admin/src/pages/ListItems/ListItems.jsx
@@ -1,107 +1,115 @@
-import React, { useEffect, useState } from 'react';
-import './ListItems.css';
-import axios from 'axios';
-import { toast } from 'react-toastify';
-import { GrEdit } from 'react-icons/gr';
-import { AiOutlineDelete } from 'react-icons/ai';
+import React, { useEffect, useState } from "react";
+import "./ListItems.css";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { GrEdit } from "react-icons/gr";
+import { AiOutlineDelete } from "react-icons/ai";
+import Navbar from "../../components/Navbar/Navbar";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import config from "../../../config.json";
 
 const ListItems = () => {
-    const url = 'http://localhost:8080';
-    const [list, setList] = useState([]);
+  const url = config.baseURL;
+  const [list, setList] = useState([]);
 
-    const fetchList = async () => {
-        try {
-            const response = await axios.get(`${url}/api/products/allProducts`);
-            console.log(response.data);
-            if (response.data.success) {
-                setList(response.data.data);
-            } else {
-                toast.error(response.data.message);
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(`${url}/api/products/allProducts`);
+      if (response.data.success) {
+        setList(response.data.data);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const removeItem = async (id) => {
-        try {
-            const response = await axios.delete(
-                `${url}/api/products/delete/${id}`
-            );
-            if (response.data.success) {
-                toast.success(response.data.message);
-                fetchList();
-            } else {
-                toast.error(response.data.message);
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    useEffect(() => {
+  const removeItem = async (id) => {
+    try {
+      const response = await axios.delete(`${url}/api/products/delete/${id}`);
+      if (response.data.success) {
+        toast.success(response.data.message);
         fetchList();
-    }, []);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    return (
-        <div className="list_items">
-            <h3 className="list_title">All List Items</h3>
+  useEffect(() => {
+    fetchList();
+  }, []);
 
-            <table className="list_table">
-                <thead className="table_header">
-                    <tr className="header_row">
-                        <th scope="col" className="header_name">
-                            Image
-                        </th>
-                        <th scope="col" className="header_name">
-                            Name
-                        </th>
-                        <th scope="col" className="header_name">
-                            Category
-                        </th>
-                        <th scope="col" className="header_name">
-                            Description
-                        </th>
-                        <th scope="col" className="header_name">
-                            Price
-                        </th>
-                        <th scope="col" className="header_name">
-                            Actions
-                        </th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    {list.map((item) => (
-                        <tr className="body_row" key={item._id}>
-                            <td>
-                                <img src={item.imageURL} alt="Item-Image" />
-                            </td>
-                            <td>{item.name}</td>
-                            <td>{item.category}</td>
-                            <td>{item.description}</td>
-                            <td>Rs. {item.price}</td>
-                            <td className="action_buttons">
-                                <div>
-                                    <button className="edit_button">
-                                        <GrEdit />
-                                    </button>
-                                </div>
-                                <div>
-                                    <button
-                                        onClick={() => removeItem(item._id)}
-                                        className="delete_button"
-                                    >
-                                        <AiOutlineDelete />
-                                    </button>
-                                </div>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+  return (
+    <>
+      <Navbar />
+      <div className="app-content">
+        <div>
+          <Sidebar />
         </div>
-    );
+        <div className="list_items">
+          <h3 className="list_title">All List Items</h3>
+
+          <table className="list_table">
+            <thead className="table_header">
+              <tr className="header_row">
+                <th scope="col" className="header_name">
+                  Image
+                </th>
+                <th scope="col" className="header_name">
+                  Name
+                </th>
+                <th scope="col" className="header_name">
+                  Category
+                </th>
+                <th scope="col" className="header_name">
+                  Description
+                </th>
+                <th scope="col" className="header_name">
+                  Price
+                </th>
+                <th scope="col" className="header_name">
+                  Actions
+                </th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {list.map((item) => (
+                <tr className="body_row" key={item._id}>
+                  <td>
+                    <img src={item.imageURL} alt="Item-Image" />
+                  </td>
+                  <td>{item.name}</td>
+                  <td>{item.category}</td>
+                  <td>{item.description}</td>
+                  <td>Rs. {item.price}</td>
+                  <td className="action_buttons">
+                    <div>
+                      <button className="edit_button">
+                        <GrEdit />
+                      </button>
+                    </div>
+                    <div>
+                      <button
+                        onClick={() => removeItem(item._id)}
+                        className="delete_button"
+                      >
+                        <AiOutlineDelete />
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ListItems;
